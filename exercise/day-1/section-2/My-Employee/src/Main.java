5866e17efca024773a53796958141c4691f94537
taufikadip <taufikadipras@gmail.com>
2024-09-05 14:23:40 +0700

    Employee

---
+++ exercise/day-1/section-2/My-Employee/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        MyEmployee myEmployee = new MyEmployee();
+        myEmployee.nama = "Adi";
+        myEmployee.npk = "68776";
+        myEmployee.salary = 999;
+        myEmployee.address = "Bintaro";
+        myEmployee.age = 24;
+
+        myEmployee.sayHello();
+    }
+}
---
+++ exercise/day-1/section-2/My-Employee/src/MyEmployee.java
@@ -0,0 +1,13 @@
+public class MyEmployee {
+    String nama;
+    String npk;
+    int salary;
+    String address;
+    int age;
+
+    void sayHello(){
+        System.out.println("Hello World");
+    }
+}
+
+
