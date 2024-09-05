1e354ad0deda96495db46f083c79c41b5c653656
taufikadip <taufikadipras@gmail.com>
2024-09-05 10:46:25 +0700

    Luas Lingkaran

--- pre-test/day-1/HelloWorld.java/src/Main.java
+++ pre-test/day-1/HelloWorld.java/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        // Hello World
         System.out.println("Hello world!");
 
+        //Data type
         String name;
         name = "Adi";
 
@@ -27,5 +30,17 @@ public class Main {
         System.out.println(phi);
         System.out.println(grade);
         System.out.println(isJavaFun);
+
+        //Luas lingkaran
+        final float myPhi = 3.14f;
+
+        int r1 = 5;
+        float luaslingkaran1 = myPhi * r1 * r1;
+
+        int r2 = 10;
+        float luaslingkaran2 = myPhi * r2 * r2;
+
+        System.out.println(luaslingkaran1);
+        System.out.println(luaslingkaran2);
     }
 }
