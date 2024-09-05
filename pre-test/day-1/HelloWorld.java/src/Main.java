a03e4fcc11bea66b6a266f936efdc026e85ddb14
taufikadip <taufikadipras@gmail.com>
2024-09-05 10:33:02 +0700

    Data Type Proof

--- pre-test/day-1/HelloWorld.java/src/Main.java
+++ pre-test/day-1/HelloWorld.java/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String name;
+        name = "Adi";
+
+        long num;
+        num = 21;
+
+        int suhu;
+        suhu = 7;
+
+        float phi;
+        phi = 3.14f;
+
+        char grade;
+        grade = 'A';
+
+        boolean isJavaFun;
+        isJavaFun = true;
+
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(suhu);
+        System.out.println(phi);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
     }
 }
