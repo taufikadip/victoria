a478f5568254b460e84d9c2270e65e5fe63a4cac
taufikadip <taufikadipras@gmail.com>
2024-09-05 14:31:43 +0700

    Employee Data

--- exercise/day-1/section-2/My-Employee/src/Main.java
+++ exercise/day-1/section-2/My-Employee/src/Main.java
@@ -8,5 +8,24 @@ public class Main {
         myEmployee.age = 24;
 
         myEmployee.sayHello();
+        System.out.println("Nama Karyawan = " + myEmployee.nama);
+        System.out.println("NPK Karyawan = " + myEmployee.npk);
+        System.out.println("Salary Karyawan = " + myEmployee.salary);
+        System.out.println("Alamat Karyawan = " + myEmployee.address);
+        System.out.println("Usia Karyawan = " + myEmployee.age);
+
+        MyEmployee myEmployee2 = new MyEmployee();
+        myEmployee2.nama = "Kevin";
+        myEmployee2.npk = "68777";
+        myEmployee2.salary = 1000;
+        myEmployee2.address = "Cilincing";
+        myEmployee2.age = 25;
+
+        myEmployee.sayHello();
+        System.out.println("Nama Karyawan = " + myEmployee2.nama);
+        System.out.println("NPK Karyawan = " + myEmployee2.npk);
+        System.out.println("Salary Karyawan = " + myEmployee2.salary);
+        System.out.println("Alamat Karyawan = " + myEmployee2.address);
+        System.out.println("Usia Karyawan = " + myEmployee2.age);
     }
 }
