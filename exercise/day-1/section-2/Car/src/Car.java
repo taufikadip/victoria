0a4eca3ab0a2024160bd430a7d7b5547b6f139b5
taufikadip <taufikadipras@gmail.com>
2024-09-05 14:13:44 +0700

    Section 2

---
+++ exercise/day-1/section-2/Car/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String model;
+    int year;
+
+    void drive() {
+        System.out.println("Driving the car");
+    }
+}
---
+++ exercise/day-1/section-2/Car/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car(); //Creating an Object
+        myCar.model = "Toyota";
+        myCar.year = 2020;
+        myCar.drive(); // Calling a method
+    }
+}
