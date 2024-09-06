f3efdb9f2f612c038f70a1a55799a565ee18f5ec
taufikadip <taufikadipras@gmail.com>
2024-09-06 11:48:44 +0700

    Get response

--- exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -1,5 +1,7 @@
 package com.example.first_application;
 
+import com.example.first_application.response.GetAssetResponse;
+import lombok.Builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Builder
 @RestController
 @SpringBootApplication
 public class FirstApplication {
@@ -16,12 +19,21 @@ public class FirstApplication {
 		}
 
 	@GetMapping
-	public String sayHello(){
-		return "Hello World";
+	public GetAssetResponse sayHello(){
+		var response = GetAssetResponse.builder().id(1L).name("Asset1").build();
+
+		response.setName("AMAN");
+
+		response.getName();
+
+		return response;
 	}
 
 	@GetMapping("/users")
 	public List<String> getUser(){
 		return List.of("Karta", "Kevin", "Bram");
 	}
+
+//	@GetMapping("/users/{id}")
+//	public ResponseEntityLong
 }
---
+++ exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/response/GetAssetResponse.java
@@ -0,0 +1,22 @@
+package com.example.first_application.response;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+public class GetAssetResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("type")
+    private String type;
+}
