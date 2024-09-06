4b9b2f2be57aee3c618bd344c0efae759ed946f2
taufikadip <taufikadipras@gmail.com>
2024-09-06 11:20:50 +0700

    MVN Hellow

---
+++ exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -0,0 +1,20 @@
+package com.example.first_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+		}
+
+	@GetMapping
+	public String sayHello(){
+		return "Hello World";
+	}
+}
